65c540962fae5592cfbb0cf783f6beaccfefe97f
mitruhaa <charonpersonal@proton.me>
2023-05-21 22:59:29 +0300

    add visits counter

--- catalog/views.py
+++ catalog/views.py
@@ -10,9 +10,12 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
